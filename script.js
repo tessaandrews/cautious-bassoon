3f875d69880d8b02ec78636d1e758a1948a4d2b5
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-09-17 15:40:05 -0400

    final javascript code editing

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
+function generatePassword() {
 
   var specialCharacters = ["!","&","@","$","*","#","(",")"];
   var numericCharacters = ["1","2","3","4","5","6","7","8","9"];
   var lowerCasedLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperCasedLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-
-
-function generatePassword() {
 
   var lengthChar = "";
   var passwordCharacters= "";
@@ -43,12 +39,14 @@ function generatePassword() {
     if (hasUpperCasedLetters) {
     passwordCharacters += upperCasedLetters;
 }
-};
 
+var passwordString = "";
+for (var i = 0; i < passwordLength; i ++) {
+  passwordString += passwordCharacters[Math.floor(Math.random() * (passwordCharacters.length))];
+}
 
+return passwordString;
 
-for (var i = 0; i < length; i ++) {
-  password = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)]
 }
 
 
@@ -58,6 +56,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
 }
 
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
