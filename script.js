b4119a4b7af5ca6bf9cb66ace3842771d972bf10
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-09-16 21:39:25 -0400

    added more code to generate password

--- script.js
+++ script.js
@@ -6,58 +6,73 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  var length = math.floor(math.random() * 129) + 8;
-  var hasSpecialCharacters = "!","%","^","&","*","(",")","_","+","~","\","|","}","[","]",",";","?","">","<","/","
-  var hasNumericCharacters = 123456789;
-  var hasLowerCasedLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var hasUpperCasedLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-=
+  var SpecialCharacters = ["!","&","@","$","*","#","(",")"];
+  var NumericCharacters = ["1","2","3","4","5","6","7","8","9"];
+  var LowerCasedLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var UpperCasedLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
   passwordText.value = password;
+
+
+  var length = "";
+  var hasLowerCasedLetters;
+  var hasUpperCasedLetters;
+  var hasSpecialCharacters;
+  var hasNumericCharacters;
 
   var passwordOptions = {
 
 
   };
 
-
-
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
+  var length = 0
+  while (length < 8 || length > 128) {
+  length = window.prompt("How many characters would you like? (8-128 Characters)");
+    console.log(number);
 
-  window.prompt("How many characters would you like? (8-128 Characters)");
-  if (confirm);
-  console.log(length);
-  
 
-  var hasSpecialCharacters
-  window.confirm("Would you like Special Characters?");
-  if (hasSpecialCharacters= true);
-  console.log(hasSpecialCharacters);
-  if (hasSpecialCharacters= false);
-  return; 
+  var hasSpecialCharacters = window.confirm("Would you like Special Characters?");
+  var hasLowerCasedLetters = window.confirm("Would you like Lowercased Letters?");
+  var hasNumericCharacters = window.confirm("Would you like Numeric Characters?");
+  var hasUpperCasedLetters = window.confirm("Would you like Uppercased numbers?");
+}
+};
 
-  var hasLowerCasedLetters
-  window.confirm("Would you like Lowercased Letters?");
-  if (hasLowerCasedLetters= true);
-  console.log();
-  if (hasLowerCasedLetters= false);
-  return;
 
-  var hasNumericCharacters
-  window.confirm("Would you like Numeric Characters?");
-  if (hasNumericCharacters= true);
-  console.log(Math.random(Math.floor()));
-  if (hasNumericCharacters= false);
-  return;
 
-  var hasUpperCasedLetters
-  window.confirm("Would you like Uppercased numbers?");
-  if (hasUpperCasedLetters = true);
-  console.log(hasUpperCasedLetters);
-  if (hasUpperCasedLetters = false);
-  return;
+if(hasNumericCharacters) {
+  passwordCharacters = 
+  passwordCharacters.concat(NumericCharacters)
+}
+if (hasUpperCasedLetters) {
+  passwordCharacters =
+  passwordCharacters.concat(UpperCasedLetters)
+}
+if (hasLowerCasedLetters) {
+  passwordCharacters =
+  passwordCharacters.concat(LowerCasedLetters)
+}
 
+console.log(passwordCharacters)
+
+var randomPassword = ""
+
+for(var i = 0; i < length; i++) {
+  randomPassword = randomPassword +
+  passwordCharacters[Math.floor(Math.random() *
+  passwordCharacters.length)];
+  }
+
+
+
+function generatePassword () {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  console.log(password)
+  passwordText.value=password;
+
+}
 }
